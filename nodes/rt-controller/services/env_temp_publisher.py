124d3dab1ef668749308cfb0d98f41ea36087546
spifford3472 <spifford@gmail.com>
2026-02-06 19:46:22 -0500

    fixes

--- nodes/rt-controller/services/env_temp_publisher.py
+++ nodes/rt-controller/services/env_temp_publisher.py
@@ -127,7 +127,7 @@ def main() -> None:
         else:
             # do NOT blank c/f; just mark stale and update timestamp
             r.hset(KEY_TEMP, mapping={
-                "source": "unknown",
+                "source": "unknown-test",
                 "stale": "1",
                 "last_update_ms": ts,
             })
